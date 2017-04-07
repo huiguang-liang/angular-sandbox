302df9f1af100a68de2751636f682e2012768c2e
huiguang-liang <neverwalkalone@gmail.com>
2017-04-07 17:11:11 +0800

    Further playing around

--- angular-tour-of-heroes/src/app/app.component.js
+++ angular-tour-of-heroes/src/app/app.component.js
@@ -8,19 +8,65 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var app_helpers_1 = require("./app.helpers");
 var H = new app_helpers_1.Helper;
+// Class declarations
+var Id = (function () {
+    function Id() {
+        this.id = 0;
+    }
+    Id.prototype.getNextId = function () {
+        return this.id++;
+    };
+    return Id;
+}());
+exports.Id = Id;
+var I = new Id;
+var Hero = (function () {
+    function Hero(n) {
+        this.id = I.getNextId();
+        this.name = n;
+    }
+    Hero.prototype.isEqual = function (h) {
+        return h.id === this.id && h.name === this.name;
+    };
+    Hero.listHeroesExcluding = function (h, r) {
+        // listHeroesExcluding(h: Hero[], excludeH: Hero[]) {
+        //var heroes = Array.from(h, x => x.valueOf());
+        // var excludeHeroes = Array.from(h, x => x.valueOf());
+        //
+        // return heroes.concat(excludeHeroes).toString();
+        // return heroes;
+        var heroes = h.filter(function (x) {
+            return !x.isEqual(r);
+        });
+        return Array.from(heroes, function (x) { return x.valueOf(); });
+    };
+    Hero.prototype.valueOf = function () {
+        return JSON.stringify({
+            id: this.id,
+            name: this.name
+        });
+    };
+    return Hero;
+}());
+exports.Hero = Hero;
 var AppComponent = (function () {
     function AppComponent() {
         this.name = 'Angular';
         this.title = 'Tour of Heroes';
-        this.heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
-        this.myHero = this.heroes[H.rand(0, this.heroes.length)];
+        this.heroes = [
+            new Hero('Windstorm'),
+            new Hero('Bombasto'),
+            new Hero('Mr. Nice'),
+        ];
+        // heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+        this.randomHero = this.heroes[H.rand(0, this.heroes.length)];
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "\n    <div class=\"row\" style=\"margin: 15px;\">\n      <div class=\"col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1\">\n        <h1>{{title}}</h1>\n        <hr>\n        <h2>My favorite hero is {{ myHero }}!</h2>\n        <div class=\"panel panel-default\">\n          <div class=\"panel-body\">\n            List of heros: <span *ngFor=\"let hero of heroes; let last = last\">{{ hero }}{{last ? '' : ', '}}</span>\n          </div>\n        </div>\n      </div>\n    </div>\n  ",
+        template: "\n    <div class=\"row\" style=\"margin: 15px;\">\n      <div class=\"col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1\">\n        <h1>{{title}}</h1>\n        <hr>\n        <div class=\"jumbotron jumbotron-add-padding\">\n          <h1>Random hero of the day is {{ randomHero.name }}!</h1>\n          <p>The other heros are: <span *ngFor=\"let hero of heroes; let last = last; let first = first; let i = index\">{{hero.valueOf()}}</span></p>\n        </div>\n      </div>\n    </div>\n  ",
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -3,6 +3,54 @@ import { Helper } from './app.helpers';
 
 let H = new Helper;
 
+// Class declarations
+export class Id {
+  private id: number = 0;
+  constructor () {
+
+  }
+  public getNextId() {
+    return this.id++;
+  }
+}
+
+let I = new Id;
+
+export class Hero {
+
+  readonly id: number;
+  public name: string;
+
+  constructor (n: string) {
+    this.id = I.getNextId();
+    this.name = n;
+  }
+
+  isEqual(h: Hero) {
+    return h.id === this.id && h.name === this.name;
+  }
+
+  public static listHeroesExcluding(h: Hero[], r: Hero) {
+  // listHeroesExcluding(h: Hero[], excludeH: Hero[]) {
+    //var heroes = Array.from(h, x => x.valueOf());
+    // var excludeHeroes = Array.from(h, x => x.valueOf());
+    //
+    // return heroes.concat(excludeHeroes).toString();
+    // return heroes;
+    var heroes = h.filter(function(x) {
+      return !x.isEqual(r);
+    });
+    return Array.from(heroes, x => x.valueOf());
+  }
+
+  valueOf() {
+    return JSON.stringify({
+      id: this.id,
+      name: this.name
+    });
+  }
+}
+
 @Component({
   selector: 'my-app',
   template: `
@@ -10,11 +58,9 @@ let H = new Helper;
       <div class="col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1">
         <h1>{{title}}</h1>
         <hr>
-        <h2>My favorite hero is {{ myHero }}!</h2>
-        <div class="panel panel-default">
-          <div class="panel-body">
-            List of heros: <span *ngFor="let hero of heroes; let last = last">{{ hero }}{{last ? '' : ', '}}</span>
-          </div>
+        <div class="jumbotron jumbotron-add-padding">
+          <h1>Random hero of the day is {{ randomHero.name }}!</h1>
+          <p>The other heros are: <span *ngFor="let hero of heroes; let last = last; let first = first; let i = index">{{hero.valueOf()}}</span></p>
         </div>
       </div>
     </div>
@@ -24,6 +70,19 @@ let H = new Helper;
 export class AppComponent {
   name = 'Angular';
   title = 'Tour of Heroes';
-  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
-  myHero = this.heroes[H.rand(0, this.heroes.length)];
+  heroes: Hero[] = [
+    new Hero('Windstorm'),
+    new Hero('Bombasto'),
+    new Hero('Mr. Nice'),
+    // new Hero('Narco'),
+    // new Hero('Celeritas'),
+    // new Hero('Magneta'),
+    // new Hero('RubberMan'),
+    // new Hero('Dynama'),
+    // new Hero('Dr. IQ'),
+    // new Hero('Magma'),
+    // new Hero('Tornado'),
+  ];
+  // heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  randomHero = this.heroes[H.rand(0, this.heroes.length)];
 }
